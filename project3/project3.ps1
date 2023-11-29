31a6ad7a0dc1991764b8afc2a07805663bb9081a
huthbn <huthbn@mail.uc.edu>
2023-11-28 22:27:27 -0500

    Update project3.ps1

--- project3/project3.ps1
+++ project3/project3.ps1
@@ -298,6 +298,89 @@ Write-Output "You are running low on space. Here are the top three files in $dri
 
 Write-Host ""
 
+# Check if the selected drive contains files
+if ($largestFile) {
+    Write-Output "Largest File:"
+    Write-Output "Name: $($largestFile.Name)"
+
+    #Add error handling for displaying bytes, MBs, or greater
+
+    if ($largestFile.Length -lt 1MB) {
+
+        Write-Output "$($largestFile.Length / 1KB) KB"
+    }
+    
+    elseif ($largestFile.Length -lt 1GB) {
+
+        Write-Output "Size: $($largestFile.Length / 1MB) MB"
+    }
+
+    else {
+        Write-Output "Size: $($largestFile.Length / 1GB) GB"
+    }
+} 
+
+
+else {
+    Write-Output "No files found on the $driveletter drive."
+}
+
+if($secondLargestFile) {
+
+Write-Output "Second Largest File:"
+Write-Output "Name: $($SecondlargestFile.Name)"
+
+#Add error handling for displaying bytes, MBs, or greater
+
+if ($secondLargestFile.Length -lt 1MB) {
+
+    Write-Output "$($secondLargestFile.Length / 1KB) KB"
+}
+
+elseif ($secondLargestFile.Length -lt 1GB) {
+
+    Write-Output "Size: $($secondLargestFile.Length / 1MB) MB"
+}
+
+else {
+    Write-Output "Size: $($secondLargestFile.Length / 1GB) GB"
+}
+
+} 
+
+
+else {
+Write-Output "No other files found on the $driveletter drive."
+}
+
+if($thirdLargestFile) {
+
+    Write-Output "Third Largest File:"
+    Write-Output "Name: $($thirdLargestFile.Name)"
+    
+    #Add error handling for displaying bytes, MBs, or greater
+    
+    if ($thirdLargestFile.Length -lt 1MB) {
+    
+        Write-Output "$($thirdLargestFile.Length / 1KB) KB"
+    }
+    
+    elseif ($thirdLargestFile.Length -lt 1GB) {
+    
+        Write-Output "Size: $($thirdLargestFile.Length / 1MB) MB"
+    }
+    
+    else {
+        Write-Output "Size: $($thirdLargestFile.Length / 1GB) GB"
+    }
+    
+    } 
+    
+    
+    else {
+    Write-Output "No other files found on the $driveletter drive."
+    }
+    
 
 
 }
